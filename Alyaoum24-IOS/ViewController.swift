2bf334b2bf8632ac3527286b80fe3c6b7c3f451b
AKRAM <208599@supinfo.com>
2016-08-16 09:31:23 +0100

    Initial Commit

---
+++ Alyaoum24-IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alyaoum24-IOS
+//
+//  Created by AKRAM on 16/08/2016.
+//  Copyright © 2016 AKRAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
